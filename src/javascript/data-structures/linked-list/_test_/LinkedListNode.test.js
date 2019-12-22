736629435ddbbdc59ac8c9a6fcc59fee44e067ce
Henrik Grönvall <hgc-ab@outlook.com>
2019-12-22 07:01:43 +0100

    Initial commit

--- src/javascript/data-structures/linked-list/_test_/LinkedListNode.test.js
+++ src/javascript/data-structures/linked-list/_test_/LinkedListNode.test.js
@@ -1,46 +1,34 @@
-import LinkedListNode from '../LinkedListNode';
+import LinkedListNode from '../LinkedListNode'
 
 describe('LinkedListNode', () => {
-  it('should create list node with value', () => {
-    const node = new LinkedListNode(1);
 
-    expect(node.value).toBe(1);
-    expect(node.next).toBeNull();
-  });
+  it('should create list node', () => {
+    const nodeValue = { name: 'Henrik', key: 'test' }
+    const node = new LinkedListNode(nodeValue)
 
-  it('should create list node with object as a value', () => {
-    const nodeValue = { value: 1, key: 'test' };
-    const node = new LinkedListNode(nodeValue);
-
-    expect(node.value.value).toBe(1);
-    expect(node.value.key).toBe('test');
-    expect(node.next).toBeNull();
-  });
-
-  it('should link nodes together', () => {
-    const node2 = new LinkedListNode(2);
-    const node1 = new LinkedListNode(1, node2);
-
-    expect(node1.next).toBeDefined();
-    expect(node2.next).toBeNull();
-    expect(node1.value).toBe(1);
-    expect(node1.next.value).toBe(2);
-  });
+    expect(node.data).toStrictEqual(nodeValue)
+    expect(node.next).toBeNull()
+  })
 
   it('should convert node to string', () => {
-    const node = new LinkedListNode(1);
+    const node1 = new LinkedListNode(1)
+    expect(node1.toValue()).toBe('1')
+  })
 
-    expect(node.toString()).toBe('1');
+  it('should convert node to string with custom callback', () => {
+    const nodeValue = { name: 'Henrik', title: 'Engineer' }
+    const node = new LinkedListNode(nodeValue)
+    const toStringCallback = data => `name: ${data.name}, title: ${data.title}`
+    expect(node.toValue(toStringCallback)).toBe('name: Henrik, title: Engineer')
+  })
 
-    node.value = 'string value';
-    expect(node.toString()).toBe('string value');
-  });
+  it('should convert node to object with custom callback', () => {
+    const nodeValue = { name: 'Henrik', title: 'Engineer' }
+    const node = new LinkedListNode(nodeValue)
+    const toObjectCallback = data => data
+    expect(node.toValue(toObjectCallback)).toStrictEqual(nodeValue)
+  })
 
-  it('should convert node to string with custom stringifier', () => {
-    const nodeValue = { value: 1, key: 'test' };
-    const node = new LinkedListNode(nodeValue);
-    const toStringCallback = value => `value: ${value.value}, key: ${value.key}`;
 
-    expect(node.toString(toStringCallback)).toBe('value: 1, key: test');
-  });
-});
+
+})
